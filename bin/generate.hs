5c071267f3dd64d500023d4be54f5bc10776a3b0
Vo Minh Thu <thu@hypered.io>
2021-10-09 13:03:07 +0200

    Add head and body tags.

--- bin/generate.hs
+++ bin/generate.hs
@@ -19,15 +19,37 @@ main = do
 
 --------------------------------------------------------------------------------
 page :: Html
-page = document $
+page = document "Smart design system" $
   return ()
 
 
 --------------------------------------------------------------------------------
-document body = do
+document title body = do
   H.docType
   H.html
     ! A.class_ "u-maximize-height"
     ! A.dir "ltr"
     ! A.lang "en" $ do
+    myHead title
+    myBody body
+
+
+--------------------------------------------------------------------------------
+myHead title =
+  H.head $ do
+    H.meta ! A.charset "utf-8"
+    H.title title
+    H.meta ! A.name "viewport" ! A.content "width=device-width, initial-scale=1"
+    H.meta ! A.name "robots" ! A.content "noindex"
+    H.link
+      ! A.rel "stylesheet"
+      ! A.href "https://design.smart.coop/css/main.css"
+
+myBody body =
+  H.body ! A.class_ "u-maximize-height u-overflow-hidden" $ do
     body
+    js
+
+js = do
+  H.script ! A.src "https://design.smart.coop/js/bundle-prototype.js" $ ""
+  H.script ! A.src "https://design.smart.coop/js/bundle-client.js" $ ""
