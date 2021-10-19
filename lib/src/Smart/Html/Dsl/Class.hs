0eac0002b03217c92b8f9ed7e4b1e8cbb00d48d9
asheshambasta <ashesh.ambasta@gmail.com>
2021-10-19 09:25:58 +0200

    Remove redundant module.

--- lib/src/Smart/Html/Dsl/Class.hs
+++
@@ -1,9 +0,0 @@
-module Smart.Html.Dsl.Class
-  () where
-
-import qualified Text.Blaze.Html               as B
-
--- | A smart design element.
-class Elem e where
-  -- | Render/draw an element as HTML 
-  renderElem :: e -> B.Html
