db56c515bc7a2098a7e9c0eda71de0976dfed8a6
asheshambasta <ashesh.ambasta@gmail.com>
2021-10-20 08:46:51 +0200

    Render: render as text.

--- lib/src/Smart/Html/Render.hs
+++ lib/src/Smart/Html/Render.hs
@@ -1,11 +1,19 @@
 module Smart.Html.Render
   ( renderCanvasWithHead
+  , renderCanvasWithHeadText
   , smartDesignHead
   ) where
 
+import qualified Data.Text                     as T
 import qualified Smart.Html.Dsl                as Dsl
+import qualified Text.Blaze.Html.Renderer.Pretty
+                                               as R
 import qualified Text.Blaze.Html5              as H
 import qualified Text.Blaze.Html5.Attributes   as A
+
+-- | Render a Smart canvas ensuring the CSS etc. are properly imported, as Text 
+renderCanvasWithHeadText :: Dsl.HtmlCanvas -> T.Text
+renderCanvasWithHeadText = T.pack . R.renderHtml . renderCanvasWithHead
 
 -- | Render a Smart canvas ensuring the CSS etc. are properly imported.
 renderCanvasWithHead :: Dsl.Canvas H.ToMarkup -> H.Html
