c79b0fff38460da3eaa61299edd2ec89ab254140
asheshambasta <ashesh.ambasta@gmail.com>
2021-10-22 12:45:46 +0200

    Cleanup rendering
    Also use all css links as in the current smart design system (some of
    the rendered components aren't working correctly.
    These need to be investigated.

--- lib/src/Smart/Html/Render.hs
+++ lib/src/Smart/Html/Render.hs
@@ -9,6 +9,7 @@ import qualified Smart.Html.Dsl                as Dsl
 import qualified Text.Blaze.Html.Renderer.Pretty
                                                as R
 import qualified Text.Blaze.Html5              as H
+import           Text.Blaze.Html5               ( (!) )
 import qualified Text.Blaze.Html5.Attributes   as A
 
 -- | Render a Smart canvas ensuring the CSS etc. are properly imported, as Text 
@@ -16,14 +17,22 @@ renderCanvasWithHeadText :: Dsl.HtmlCanvas -> T.Text
 renderCanvasWithHeadText = T.pack . R.renderHtml . renderCanvasWithHead
 
 -- | Render a Smart canvas ensuring the CSS etc. are properly imported.
-renderCanvasWithHead :: Dsl.Canvas H.ToMarkup -> H.Html
-renderCanvasWithHead canvas = smartDesignHead >> H.body (H.toMarkup canvas)
+renderCanvasWithHead :: Dsl.HtmlCanvas -> H.Html
+renderCanvasWithHead canvas = smartDesignHead >> body
+  where body = H.body (H.toMarkup canvas) ! A.class_ "u-maximize-height"
 
 -- | Markup for the smart CSS head etc.
 smartDesignHead :: H.Html
 smartDesignHead =
-  H.head
-    $   H.link
-    H.! (A.rel "stylesheet")
-    H.! (A.href "https://design.smart.coop/css/main.css")
+  H.head $ maincss >> protocss >> custscss >> charset >> viewport
+ where
+  maincss  = stylesheet "main.css"
+  protocss = stylesheet "prototype.css"
+  custscss = stylesheet "styleguide-customizations.css"
+
+  stylesheet file = H.link ! A.rel "stylesheet" ! A.href
+    ("https://design.smart.coop/css/" <> file)
+  charset = H.meta ! A.charset "utf-8"
+  viewport =
+    H.meta ! A.name "viewport" ! A.content "width=device-width, initial-scale=1"
 
