43b49fd0bf7607da75c1b9eaa9a7c4477c52fba8
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2021-12-26 16:21:58 +0100

    Added docs to clarify FilesystemConf
    @thusc in response to https://github.com/smartcoop/design-hs/pull/1#discussion_r775218689

--- exe/bin/Conf/Types.hs
+++ exe/bin/Conf/Types.hs
@@ -7,10 +7,40 @@ module Conf.Types
 import           Control.Lens
 import qualified Data.Text                     as T
 
--- | Run configuration
+{- | Run configuration: filesystem.
+
+The configuration has 2 parts:
+
+1. The root directory under which to place the index.html file. 
+
+2. The examples subdirectory (non-absolute) under which to place examples. 
+
+--
+
+For example: if we have:
+
+@
+FilesystemConf
+{ _fcOutputDir      = "/foo/bar"
+, _fcExamplesSubdir = "baz"
+}
+@
+
+We'll end up with the following tree:
+
+@
+foo
+└── bar
+    ├── examples
+    │   └── some-example0.html
+    └── index.html
+@
+
+The examples subdirectory will be created if it doesn't exist yet; and existing files will be overwritten. 
+-}
 data FilesystemConf = FilesystemConf
-  { _fcOutputDir      :: FilePath
-  , _fcExamplesSubdir :: FilePath
+  { _fcOutputDir      :: FilePath -- ^ The output directory: the index.html file is placed here. 
+  , _fcExamplesSubdir :: FilePath -- ^ The examples subdirectory /under/ the output directory. This /must not/ be absolute. 
   }
   deriving Show
 
